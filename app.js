85fd4d0af4ad9f14f0eb34f4c4ab01116775d55c
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-10-03 14:19:48 +0800

    Add CORS headers to allow cross-origin requests

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/img', express.static(path.join(__dirname, '/public/img')));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 
 const db = require('./app/models');
 db.mongoose
